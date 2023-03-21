bc8d00ad82a425a76f3cb1cb31927b28c89b589d
Excell-09 <excell.agcb12@gmail.com>
2023-03-21 22:39:35 +0700

    deploy

--- src/utils/setCookie.tsx
+++ src/utils/setCookie.tsx
@@ -2,7 +2,11 @@ const setCookie = (name: string, value: string) => {
   const date = new Date();
   date.setTime(date.getTime() + 24 * 60 * 60 * 1000);
   const expires = `expires=${date.toUTCString()}`;
-  document.cookie = `${name}=${value};${expires};domain=my-shop-self.vercel.app;path=/`;
+  let cookieValue = `${name}=${value};${expires};domain=my-shop-self.vercel.app;path=/;SameSite=Lax`;
+  if (window.location.protocol === 'https:') {
+    cookieValue += ';Secure';
+  }
+  document.cookie = cookieValue;
 };
 
 export default setCookie;
