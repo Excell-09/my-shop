df69f3eae06f2cc247144a489f0446dae165163b
Excell-09 <excell.agcb12@gmail.com>
2023-04-23 19:09:47 +0700

    reload when login and register

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import Logo from '../assets/favicon-32x32.png';
+import { useRouter } from 'next/router';
 
 const Footer = () => {
   const dateNow = new Date().getFullYear();
+  const router = useRouter();
   return (
     <footer className='bg-white mt-5 px-3 py-5 sm:p-6 text-sm md:text-base'>
       <div className='grid grid-cols-2 sm:grid-cols-4 gap-5 mb-7'>
@@ -48,21 +50,29 @@ const Footer = () => {
         <div>
           <h3 className='text-pink-500 text-xl font-bold mb-3'>Quick Started</h3>
           <div className='flex flex-col gap-1'>
-            <Link
-              className='hover:text-indigo-500 transition duration-100 text-gray-600 font-semibold'
-              href={'/register'}>
+            <button
+              onClick={() => {
+                router.push('/register');
+                window.location.reload();
+              }}
+              className='hover:text-indigo-500 transition duration-100 text-gray-600 font-semibold'>
               Register
-            </Link>
-            <Link
-              className='hover:text-indigo-500 transition duration-100 text-gray-600 font-semibold'
-              href={'/login'}>
+            </button>
+            <button
+              onClick={() => {
+                router.push('/login');
+                window.location.reload();
+              }}
+              className='hover:text-indigo-500 transition duration-100 text-gray-600 font-semibold'>
               Login
-            </Link>
+            </button>
           </div>
         </div>
       </div>
       <hr className=' border-t-2 border-indigo-500 my-3' />
-      <p className='text-gray-700 text-center'>&#169; {dateNow} Jun Choi, inc. All rights reserved</p>
+      <p className='text-gray-700 text-center'>
+        &#169; {dateNow} Jun Choi, inc. All rights reserved
+      </p>
     </footer>
   );
 };
